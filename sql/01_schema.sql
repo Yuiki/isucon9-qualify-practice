4cda1985e0ab5c8a95ea2a4e2e99d94b39ac8ef2
Yuiki <10101661+Yuiki@users.noreply.github.com>
2021-06-23 19:10:26 +0900

    add idx_sel_sta_cre index to items

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -33,7 +33,8 @@ CREATE TABLE `items` (
   INDEX idx_category_id (`category_id`),
   INDEX idx_sta_cat_cre (`status`, `category_id`, `created_at`),
   INDEX idx_sta_cre (`status`, `created_at`),
-  INDEX idx_created_at (`created_at`)
+  INDEX idx_created_at (`created_at`),
+  INDEX idx_sel_sta_cre (`seller_id`, `status`, `created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `transaction_evidences`;
